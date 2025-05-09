824ebcba71afc8c64bc86bdd36a8209f6335d0ca
TroyKDuncan <troyduncanbusiness@gmail.com>
2025-05-09 16:25:30 -0500

    Okay, now it's REALLY done... i think

--- Onboarding/src/main.ts
+++ Onboarding/src/main.ts
@@ -7,11 +7,16 @@
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app/app.routes'; // Make sure this path is correct
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })
+    ),
+  ],
 }).catch((err) => {
   console.error(err);
 });
